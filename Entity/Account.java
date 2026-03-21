bd169736c24112187466114934537b39f01db3d9
yashwant-code149 <narvariyayashwant@gmail.com>
2026-03-21 12:07:12 +0530

    Add files via upload

---
+++ Entity/Account.java
@@ -0,0 +1,27 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "accounts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id ;
+	private String accountHolderName ;
+	private double balance;
+	
+	
+	
+
+}
